e876c9aa49dfac22dd477f2c8d456aeb3d4f9fe8
lumentae <66220444+lumentae@users.noreply.github.com>
2025-11-22 20:43:26 +0100

    Add more docker compose files (grafana, loki, prometheus, influxdb + glance dashboard)

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -2,5 +2,5 @@ require("nvchad.configs.lspconfig").defaults()
 
 local servers = { "html", "cssls" }
 vim.lsp.enable(servers)
-
+vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
 -- read :h vim.lsp.config for changing options of lsp servers 
