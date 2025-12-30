6dceeb13d749d9348d136ccfa48a999de7e4a8b5
lumentae <me@lumentae.dev>
2025-12-30 15:50:00 +0100

    Add github copilot plugin

---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "github/copilot.vim",
+    lazy = false,
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    opts = {
+      -- See Configuration section for options
+    },
+    lazy = false,
+  },
+}
