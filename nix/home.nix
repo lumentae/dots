294e372ba1bb2a8c03cf7dabe15c1d31458aca01
lumentae <me@lumentae.dev>
2026-05-01 11:48:40 +0200

    Update nix nvidia driver version

--- nix/home.nix
+++ nix/home.nix
@@ -8,8 +8,8 @@
     enable = true;
     gpu.nvidia = {
       enable = true;
-      version = "595.58.03";
-      sha256 = "sha256-jA1Plnt5MsSrVxQnKu6BAzkrCnAskq+lVRdtNiBYKfk=";
+      version = "595.71.05";
+      sha256 = "sha256-NiA7iWC35JyKQva6H1hjzeNKBek9KyS3mK8G3YRva4I=";
     };
   };
 
