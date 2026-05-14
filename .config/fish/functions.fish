2c55734fdd85074b04071a67f5f29fd5efce466a
lumentae <me@lumentae.dev>
2026-05-15 01:03:30 +0200

    Remove pinned commit from updatePearDesktop function

--- .config/fish/functions.fish
+++ .config/fish/functions.fish
@@ -6,7 +6,7 @@ end
 function updatePearDesktop
     git clone https://github.com/pear-devs/pear-desktop
     cd pear-desktop
-    git checkout 90a31daf24f53528e1f3086bfdd5544999a34f65
+    # git checkout 90a31daf24f53528e1f3086bfdd5544999a34f65
     pnpm install --frozen-lockfile
     pnpm dist:linux
     cp pack/YouTube\ Music-3.11.0.AppImage ~/.local/bin/pear-desktop/YouTubeMusic.AppImage
