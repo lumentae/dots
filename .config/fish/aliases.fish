bfd0ef28eb4d03c541d32e93b1a5ccc83f8ff83c
lumentae <me@lumentae.dev>
2026-07-11 01:27:43 +0200

    Fix nix rebuild alias

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -1,4 +1,4 @@
 alias ls 'eza --icons -lag'
 alias clear "printf '\033[2J\033[3J\033[1;1H'"
-alias nr 'home-manager switch --flake /home/lumentae/dots/nix#lumentae-arch --impure'
+alias nr 'sudo nixos-rebuild switch --flake /home/lumentae/dots/nix#lumentae-nixos --impure'
 alias rw "sudo bootctl set-oneshot auto-windows && reboot"
