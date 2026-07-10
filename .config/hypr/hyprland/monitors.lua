9250262e0f93fc05aaff125e540cdb6185eef8fb
lumentae <me@lumentae.dev>
2026-07-11 01:22:54 +0200

    Fix monitor layout for hyprland

--- .config/hypr/hyprland/monitors.lua
+++ .config/hypr/hyprland/monitors.lua
@@ -1,13 +1,13 @@
 hl.monitor({
     output   = "desc:Acer Technologies ED320QR S3 P4220F7853W01",
     mode     = "1920x1080@180",
-    position = "auto",
-    scale    = "auto",
+    position = "0x0",
+    scale    = 1,
 })
 
 hl.monitor({
     output   = "desc:Acer Technologies ED320QR P 322503FF13W01",
     mode     = "1920x1080@165",
-    position = "auto-right",
-    scale    = "auto",
+    position = "1920x0",
+    scale    = 1,
 })
