749ae7c52ddc73e1d1d7a859cae0a3bacf71e5a1
lumentae <me@lumentae.dev>
2026-07-11 01:20:55 +0200

    Update matugen theme

--- .config/hypr/hyprland/colors.lua
+++ .config/hypr/hyprland/colors.lua
@@ -1,16 +1,16 @@
 hl.config({
     general = {
         col = {
-            active_border   = "rgba(46464b77)",
-            inactive_border = "rgba(1c1b1c33)",
+            active_border   = "rgba(434750FF)",
+            inactive_border = "rgba(1a1b1f33)",
         },
     },
     misc = {
-        background_color = "rgba(131314FF)",
+        background_color = "rgba(121317FF)",
     },
 })
 
 hl.window_rule({
     match        = { pin = 1 },
-    border_color = "rgba(c4c6d2AA) rgba(c4c6d277)",
+    border_color = "rgba(acc7ffAA) rgba(acc7ff77)",
 })
